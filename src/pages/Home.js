5254115684845dd012d3d7581da977b1cf32515b
BogDone <buselb2@gmail.com>
2021-05-29 12:26:42 +0300

    add animations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { Notes } from '../components/Notes';
 import {FirebaseContext} from '../context/firebase/firebaseContext';
 
 export const Home = () => {
- const {loading, notes, fetchNotes} = useContext(FirebaseContext);
+ const {loading, notes, fetchNotes, removeNote} = useContext(FirebaseContext);
 
  useEffect(() => {
   fetchNotes()
@@ -20,7 +20,7 @@ export const Home = () => {
 
    {loading
     ? <Loader />
-    : <Notes notes={notes} />
+    : <Notes notes={notes} onRemove={removeNote} />
    }
 
   </Fragment>
